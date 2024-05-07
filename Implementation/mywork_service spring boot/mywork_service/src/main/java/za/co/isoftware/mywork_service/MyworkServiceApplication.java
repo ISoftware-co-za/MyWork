889a82ab0cbf7fef86aca600327fd306c55c5cac
Keith <keithc@isoftware.co.za>
2024-05-07 17:32:12 +0200

    Added applications and services.

---
+++ Implementation/mywork_service spring boot/mywork_service/src/main/java/za/co/isoftware/mywork_service/MyworkServiceApplication.java
@@ -0,0 +1,13 @@
+package za.co.isoftware.mywork_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyworkServiceApplication.class, args);
+	}
+
+}
---
+++ Implementation/mywork_service spring boot/mywork_service/src/test/java/za/co/isoftware/mywork_service/MyworkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.isoftware.mywork_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
