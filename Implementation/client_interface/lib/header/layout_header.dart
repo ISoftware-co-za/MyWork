cf921dc54bb6e2b0ddba1b479154659693a444bc
Keith Crompton <keithc@isoftware.co.za>
2025-06-04 05:29:54 +0200

    MW-178: Completed commit

--- Implementation/client_interface/lib/header/layout_header.dart
+++ Implementation/client_interface/lib/header/layout_header.dart
@@ -1,7 +1,10 @@
 import 'package:client_interfaces1/theme_extension_control_work_button.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../app/provider_state_application.dart';
 import '../custom_theme_data_app_header.dart';
+import '../model/work.dart';
 import '../theme_extension_app_header.dart';
 import 'layout_work_selector.dart';
 import 'control_search.dart';
@@ -16,6 +19,8 @@ class LayoutHeader extends StatelessWidget {
     ThemeData theme = CustomThemeDataAppHeader.getTheme();
     ThemeExtensionAppHeader appHeaderThemeExtension = theme.extension<ThemeExtensionAppHeader>()!;
     ThemeExtensionControlWorkButton workButtonTheme = theme.extension<ThemeExtensionControlWorkButton>()!;
+    ValueListenable<Work?> selectedWork = ProviderStateApplication.of(context)!.workController.selectedWork;
+
     return Theme(
       data: theme,
       child: Container(
@@ -26,7 +31,7 @@ class LayoutHeader extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Expanded(flex: 1, child: LayoutWorkSelector(label: 'Select work', workButtonTheme: workButtonTheme)),
+            Expanded(flex: 1, child: LayoutWorkSelector(selectedWork: selectedWork, workButtonTheme: workButtonTheme)),
             Expanded(flex: 1, child: ControlSearch()),
             Spacer(flex: 1,),
             ControlNotifications(),
